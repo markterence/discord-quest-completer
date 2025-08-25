93b445a3daa33c16da203bda8c50a2bb4905a907
Mark Terence Tiglao <github@markterence.me>
2025-08-25 17:31:30 +0800

    feat: dummy window application .NET WinForms C#

---
+++ dummy-window/dummy-window/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dummy_window
+{
+    public partial class Form1 : Form
+    {
+        private string _theme = "light";
+        private string _title_args = "name";
+
+        public string theme
+        {
+            get => _theme;
+            set
+            {
+                _theme = value.ToLower(); 
+                ApplyTheme();
+            }
+        }
+
+        public string TitleArg
+        {
+            get => _title_args;
+            set
+            {
+                _title_args = value;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyTheme()
+        {
+            bool dark = _theme == "dark";
+            BackColor = dark ? Color.FromArgb(30, 30, 30) : SystemColors.Control;
+            ForeColor = dark ? Color.Gainsboro : SystemColors.ControlText;
+
+            foreach (Control c in Controls)
+            {
+                c.BackColor = BackColor;
+                c.ForeColor = ForeColor;
+                if (c is LinkLabel link)
+                {
+                    if (dark)
+                    {
+                        link.LinkColor = Color.DeepSkyBlue;
+                        link.ActiveLinkColor = Color.LightBlue;
+                        link.VisitedLinkColor = Color.MediumPurple;
+                        link.DisabledLinkColor = Color.Gray;
+                    }
+                    else
+                    {
+                        link.LinkColor = Color.Blue;
+                        link.ActiveLinkColor = Color.Red;
+                        link.VisitedLinkColor = Color.Purple;
+                        link.DisabledLinkColor = SystemColors.GrayText;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = TitleArg + "\r\n" + "Discord Quest Completer Window";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                VisitLink();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+
+        public void VisitLink()
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/markterence/discord-quest-completer");
+        }
+    }
+}
---
+++ dummy-window/dummy-window/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dummy_window
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            string title = "Dummy Window (runner)";
+            string theme = "dark";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "--title" && i + 1 < args.Length)
+                {
+                    title = args[i + 1];
+                    i++;
+                }
+
+                if (args[i] == "--theme" && i + 1 < args.Length) 
+                { 
+                    theme = args[i + 1];
+                    i++;
+                }
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+            Form1 form = new Form1();
+            form.Text = title;
+            form.Width = 640;
+            form.Height = 480;
+            form.theme = theme; 
+
+            Application.Run(form);
+        }
+    }
+}
