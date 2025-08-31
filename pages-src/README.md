74fa90c9476ec483cdebe2b3188433e2c8ed0e3d
Mark Terence Tiglao <github@markterence.me>
2025-08-31 23:16:54 +0800

    chore: add node script to generate main.json

---
+++ pages-src/README.md
@@ -0,0 +1,4 @@
+# pages-src
+
+This contains some files that are published to github-pages as static assets.
+
---
+++ pages-src/gen-main.mjs
@@ -0,0 +1,26 @@
+import fs from 'node:fs';
+
+const DISCORD_API_VERSION = '/v9';
+const buildDiscordAPIUrl = (ver = '') => {
+    return [
+        'http://',
+        'discord.com',
+        '/api',
+        `${DISCORD_API_VERSION}`,
+        '/applications',
+        '/detectable'
+    ];
+}
+const buildDetectableAPIUrl = (ver = '') => {
+    return `https://markterence.github.io/discord-quest-completer/detectable${ver}.json`;
+}
+
+const data = {
+    updatedAt: new Date().valueOf(),
+    DISCORD_DETECTABLE_API_URL_VERSIONED: buildDiscordAPIUrl(DISCORD_API_VERSION).join(''),
+    DISCORD_DETECTABLE_DISCORD_API_URL: buildDiscordAPIUrl().join(''),
+    DETECTABLE_API_URL_MIRROR_1: buildDetectableAPIUrl(),
+    DETECTABLE_API_URL_MIRROR_1_VERSIONED: buildDetectableAPIUrl('_v9'),
+}
+
+fs.writeFileSync('./main.json', JSON.stringify(data, null, 2), { encoding: 'utf-8' });
