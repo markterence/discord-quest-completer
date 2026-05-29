4c3608fde3fc2aa7de8fbd3c97c24758cbbfd521
Mark Terence Tiglao <github@markterence.me>
2026-05-30 02:13:13 +0800

    refactor: clean up fetch_gamelist_gh_mirror function and improve error handling

--- src-tauri/src/commands/api.rs
+++ src-tauri/src/commands/api.rs
@@ -3,34 +3,33 @@ use tauri_plugin_http::reqwest::Response;
 use tauri_plugin_http::reqwest::Error as ReqwestError;
 use tauri::ipc::Response as IpcResponse;
 
+const DEV_FLAG_TEST_ERROR: bool = false;
+
+
 #[tauri::command(rename_all = "snake_case")]
 pub async fn fetch_gamelist_gh_mirror() -> Result<IpcResponse, String> {
-    // let res: Response = reqwest::get("https://markterence.github.io/discord-quest-completer/detectable.json")
-    //     .await
-    //     .map_err(|err| err.to_string())?;
+    // if else for some development flags to test error handling without making an actual network request
+    if DEV_FLAG_TEST_ERROR {
+        fetch_sample_dns_error().await?;
+    }
 
-    // Ok(IpcResponse::new(res.text().await.unwrap()))
-    fetch_sample_dns_error().await
-}
-
-#[tauri::command(rename_all = "snake_case")]
-pub async fn fetch_gamelist_from_discord() -> Result<IpcResponse, String> {
     let res: Response = reqwest::get("https://markterence.github.io/discord-quest-completer/detectable.json")
         .await
         .map_err(|err| err.to_string())?;
 
     Ok(IpcResponse::new(res.text().await.unwrap()))
-    // let res: Response = reqwest::get("https://discord.com/api/applications/detectable")
-    //     .await
-    //     .map_err(|err: ReqwestError| err.to_string())?;
+}
 
-    // Ok(IpcResponse::new(res.text().await.unwrap()))
+#[tauri::command(rename_all = "snake_case")]
+pub async fn fetch_gamelist_from_discord() -> Result<IpcResponse, String> {
+    let res: Response = reqwest::get("https://discord.com/api/applications/detectable")
+        .await
+        .map_err(|err: ReqwestError| err.to_string())?;
+
+    Ok(IpcResponse::new(res.text().await.unwrap()))
 }
 
 #[tauri::command(rename_all = "snake_case")]
 pub async fn fetch_sample_dns_error() -> Result<IpcResponse, String> {
-    let res: Response = reqwest::get("https://nonexistent.domain.example")
-        .await
-        .map_err(|err| err.to_string())?;
-    Ok(IpcResponse::new(res.text().await.unwrap()))
+    Ok(Err("test error".to_string())?) 
 }
